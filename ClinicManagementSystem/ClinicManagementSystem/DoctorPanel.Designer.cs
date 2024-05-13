7e6a8f2e0d4c98dd236ca4bf00bc124a582b032e
Maryam189 <maryamkhalid590@gmail.com>
2024-05-13 23:47:40 +0500

    updated

---
+++ ClinicManagementSystem/ClinicManagementSystem/DoctorPanel.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ClinicManagementSystem
+{
+    partial class DoctorPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoctorPanel));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // DoctorPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(792, 461);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "DoctorPanel";
+            this.Text = "DoctorPanel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ ClinicManagementSystem/ClinicManagementSystem/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace ClinicManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = ClinicManagementSystem.Properties.Resources.ConnectionString;
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand command = con.CreateCommand();
+            command.CommandText = "SELECT user_id FROM [user] WHERE user_username=@username AND user_password=@password";
+            command.Parameters.AddWithValue("@username", richTextBox1.Text);
+            command.Parameters.AddWithValue("@password", richTextBox2.Text);
+            con.Open();
+            var result = command.ExecuteScalar();
+            if (result != null)
+            {
+                // authenticated
+                if(richTextBox1.Text == "admin")
+                {
+                    // admin panel
+                    Hide();
+                    AdminPanel admin = new AdminPanel();
+                    admin.ShowDialog();
+                    Show();
+                  //  con.Close();
+                }
+                else
+                {
+                //    con.Open();
+                    command.CommandText = "SELECT account_id, account_type FROM account WHERE account_user_id=@user_id";
+                    command.Parameters.AddWithValue("@user_id", result.ToString());
+                    SqlDataReader reader = command.ExecuteReader();
+                    
+                    if(reader.Read())
+                    {
+                        int account_id = reader.GetInt32(0);
+                        int account_type = reader.GetInt32(1);
+                        con.Close();
+
+                        if (account_type == 2)
+                        {
+                            //Secertary panel
+                            Hide();
+                            SecretaryPanel secretary = new SecretaryPanel(account_id);
+                            secretary.ShowDialog();
+                            Show();
+
+                        }
+                        else if(account_type == 3)
+                        {
+                            // Doctor panel
+                            Hide();
+                            DoctorPanel doctor = new DoctorPanel(account_id);
+                            doctor.ShowDialog();
+                            Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("else");
+                        }
+
+                    }
+
+                }
+            }
+            else
+            {
+                // Authentication error
+                MessageBox.Show("Authentication Failed!");
+            }
+        }
+    }
+}
